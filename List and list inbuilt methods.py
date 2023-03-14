250c12ce5316f0040287ec57d4e59935ffa24852
prasanth <prasanthbr32@gmail.com>
2023-03-14 20:21:12 +0530

    C:\Users\Dreams Tattooing\Desktop\Jspiders\Core python\python\1.py

--- List and list inbuilt methods.py
+++
@@ -1,8 +0,0 @@
-L = ['NO','YES',15,16,17]
-L.append('str')
-L.extend((2,1,'str',(7)))
-L.insert(1,900)
-L.extend((2,))
-
-print(L)
-
--- inbuilt functions.py
+++ inbuilt functions.py
@@ -1,3 +1,13 @@
+L = ['NO','YES',15,16,17]
+L.append('str')
+L.extend((2,1,'str',(7)))
+L.insert(1,900)
+L.extend((2,))
+
+print(L)
+
+
+
 L = ['we','are','ganjini']
 print('@')
 print('@'.join(L))
