8cecdc873b01be511a09a6ccd7639b93a4932bb8
prasanth <prasanthbr32@gmail.com>
2023-03-15 15:39:24 +0530

    C:\Users\Dreams Tattooing\Desktop\Jspiders\Core python\python\.idea\workspace.xml

---
+++ PROGRAMS/pg_2.py
@@ -0,0 +1,12 @@
+# WAP check the two numbers
+a,b,c=23,130,78
+if a>b:
+    if a>c:
+        print('a is heighest')
+    else:
+        print('c is highest')
+else:
+    if b>c:
+        print('b is highest')
+    else:
+        print('c is highest')
---
+++ PROGRAMS/pg_3.py
@@ -0,0 +1,81 @@
+'''
+a,b,c,d,e=100,20,30,40,50
+
+if a>b:
+    if a>c:
+        if a>d:
+            if a>e:
+                print('a is highest')
+            else:
+                print('e is highest')
+        else:
+            if d>e:
+                print('d is highest')
+            else:
+                print('e is highest')
+    else:
+        if c>d:
+            if c>e:
+                print('c is highest')
+            else:
+                print('e is highest')
+        else:
+            if d>e:
+                print('d is highest')
+            else:
+                print('e is highest')
+else:
+    if b>c:
+        if b>d:
+            if b>e:
+                print('b is highest')
+            else:
+                print('e is highest')
+        else:
+            if d>e:
+                print('d is highest')
+            else:
+                print('e is highest')
+    else:
+        if c>d:
+            if c>e:
+                print('c is highest')
+            else:
+                print('e is highest')
+        else:
+            if d>e:
+                print('d is highest')
+            else:
+                print('e is highest')
+
+
+'''
+a,b,c,d=58,90,70,89
+
+if a>b:
+    if a>c:
+        if a>d:
+            print('a is highest')
+        else:
+            print('d id highest')
+    else:
+        if c>d:
+            print('c is highest')
+        else:
+            print('d1 is highest')
+else:
+    if b>c:
+        if b>d:
+            print('b is highest')
+        else:
+            print('d2 is highest')
+    else:
+        if c>d:
+            print('c1 is highest')
+        else:
+            print('d3 is highest')
+
+x,y,z=344,67,88
+if x>y:
+    if x>z:
+        print('x is ')
