d24afe9d67a3f73370d9bd49dbe0c6253d1b2bcb
prasanth <prasanthbr32@gmail.com>
2023-03-16 23:11:48 +0530

    C:\Users\Dreams Tattooing\Desktop\Jspiders\Core python\python\.idea\workspace.xml

--- PROGRAMS/pg_4.py
+++ PROGRAMS/pg_4.py
@@ -1,19 +1,5 @@
-'''
-year = 2020
+year = 2000
 if (year%4==0 and year%100==0 and year%400==0):
     print('Leap Year')
 else:
     print('Not Leap Year')
-
-'''
-year = int(input("Enter  a year:"))
-if (year % 4==0):
-    if (year % 100==0):
-        if (year % 400==0):
-            print(year,'Leap year')
-        else:
-            print(year,'not Leap year')
-    else:
-        print(year,'Not leap year')
-else:
-    print(year,'Not leap Year')
