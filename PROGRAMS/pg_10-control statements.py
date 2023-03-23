ee7d8689ffb1a787e3675da6d280d547cc99201c
prasanth <prasanthbr32@gmail.com>
2023-03-23 16:08:30 +0530

    Create pg_10-control statements.py

---
+++ PROGRAMS/pg_10-control statements.py
@@ -0,0 +1,34 @@
+# control statements
+# write a control break statements
+
+# for vn in range(10):
+#     if ( vn == 6):
+#         break
+#     print(vn)
+# print('hi')
+
+# Note:  if we put below break statements is getting out of below range 5.
+
+# for vn1 in range(10):
+#     print(vn1)
+#     if ( vn1 == 6):
+#         break
+# print('heloo')
+
+# Note:
+
+# vn2 = 2
+# while (vn2 != 10):
+#     if ( vn2 == 8 ):
+#         break
+#     print(vn2)
+#     vn2 +=1
+# print('i am there')
+
+vn3 = 2
+while (vn3 != 10):
+    print(vn3)
+    if ( vn3 == 8 ):
+        break
+    vn3 +=1
+print('i am there')
