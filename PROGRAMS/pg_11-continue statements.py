fc26db808048acee711dbacf994763cccf6c572b
prasanth <prasanthbr32@gmail.com>
2023-03-24 00:01:12 +0530

    Create pg_11-continue statements.py

---
+++ PROGRAMS/pg_11-continue statements.py
@@ -0,0 +1,37 @@
+
+# continue statements
+
+# print to 1 7
+
+# for a in range(1,7):
+#     if ( a ==3 or a == 5):
+#         continue
+#     print(a)
+# print('hey')
+
+# this program for
+# we understand the where are iteration and contorl flow.
+# for a in range(1,7):
+#     if ( a ==3 or a == 5):
+#       print('hi')
+#       continue
+#     print(a)
+# print('hey')
+
+# infinite loops in continue statements
+
+# num = 2
+# while (num <= 8):
+#     if (num ==5):
+#         continue
+#     print(num)
+#     num += 2
+# print('python is easy')
+
+num = 2
+while (num <= 8):
+    if (num ==5):
+        continue
+    print(num)
+    num += 2
+print('python is easy')
