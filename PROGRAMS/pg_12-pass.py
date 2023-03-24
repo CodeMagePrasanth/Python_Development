1a04007d7395682202a1ea2981172f75203a4a88
prasanth <prasanthbr32@gmail.com>
2023-03-24 16:16:09 +0530

    C:\Users\Dreams Tattooing\Desktop\Jspiders\Core python\python\PROGRAMS\pg_10-break.py

---
+++ PROGRAMS/pg_12-pass.py
@@ -0,0 +1,6 @@
+# pass
+
+for vn in range(1,7):
+    if vn == 4:
+     pass
+    print(vn)
---
+++ PROGRAMS/pg_14-print fucntion.py
@@ -0,0 +1,9 @@
+#print functions
+
+# /x ='1,2,3'
+# print(id(x))
+# print(type(x))
+# print(x)
+
+import sys
+sys.stdout.write('hi')
