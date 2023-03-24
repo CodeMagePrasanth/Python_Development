e471dff10e6b40a118a3dad634d0e84d07daa5f6
prasanth <prasanthbr32@gmail.com>
2023-03-24 20:47:04 +0530

    C:\Users\Dreams Tattooing\Desktop\Jspiders\Core python\python\PROGRAMS\pg_13-pattern1.py

---
+++ PROGRAMS/pg_14-pattern1.py
@@ -0,0 +1,12 @@
+# To write pgm 5 star same line  using for loop
+
+# for s in range(5):
+#     print('* ', end = '')
+
+# To write pgm  5 star in same line using while loop
+
+# star = 1
+# while ( star != 6):
+#     print('*', end = ' ')
+#     star += 1
+
---
+++ PROGRAMS/pg_15-pattern.py
@@ -0,0 +1,7 @@
+
+# Reverse order
+
+SR = 10
+while (SR !=0):
+    print('*' * SR)
+    SR -= 1
