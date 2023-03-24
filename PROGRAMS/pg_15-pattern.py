44f9c92dddf38ce07e7fad2aa1519c3ef856c0e3
prasanth <prasanthbr32@gmail.com>
2023-03-24 21:21:53 +0530

    Update pg_15-pattern.py

--- PROGRAMS/pg_15-pattern.py
+++ PROGRAMS/pg_15-pattern.py
@@ -1,3 +1,19 @@
+# To write pgm to print 1 to 5 star in new line using for loop
+
+for s in range(1,10):
+     print('*' * s)
+
+# Reverse order
+
+for S in range(10,0,-1):
+    print('*' *S)
+
+# To write pgm to print 1 to 5 star in new line using while loop
+
+S = 1
+while (S != 11):
+    print('*' * S)
+    S += 1
 
 # Reverse order
 
