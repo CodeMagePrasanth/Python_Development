9174b2ef99f466eba44c2ae66902719c623807ce
prasanth <prasanthbr32@gmail.com>
2023-03-27 16:07:58 +0530

    C:\Users\Dreams Tattooing\Desktop\Jspiders\Core python\python\PROGRAMS\pg_17-pattern.py

--- PROGRAMS/pg_17-pattern.py
+++ PROGRAMS/pg_17-pattern.py
@@ -1,4 +1,31 @@
-for i in range(1,4):
-    print('*' * (i))
-for j in range(2,0,-1):
-    print('*' * (j))
+#  half diamind pattern program
+
+# *
+# **
+# ***
+# **
+# *
+
+# to print using 'for' loop
+# way1
+# for i in range(1,4):
+#     print('*' * (i))
+# for j in range(2,0,-1):
+#     print('*' * (j))
+
+# 2nd way
+
+n = 5
+b = 1
+for a in range(1,n+1):
+    print('* ' * b)
+
+
+
+# 3rd way
+
+# n = 5
+# b = 1
+# for a in range(1,n+1):
+#     print('* ' * b)
+#     b = b+1 if a<n//2+1 else b-1
---
+++ PROGRAMS/pg_18-patten3.py
@@ -0,0 +1,17 @@
+#  - - *
+#  - * * *
+#  * * * * *
+#  - * * *
+#  - - *
+
+# n=5 (iteration) ,
+# space 2 , '*' 1
+# space 1 , '*' 3
+# space 0 , '*' 5
+# space 1 , '*' 3
+# space 2 , '*' 1
+
+n = 5
+for a in range(1,4,-1):
+    for b in range(b,):
+        print('{}{}'.format('  ' * (n-b),'* ' * (b)))
