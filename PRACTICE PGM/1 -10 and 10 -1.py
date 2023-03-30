3c3c1bdea17d2442e3f541f1b347f0aeda7dcd82
prasanth <prasanthbr32@gmail.com>
2023-03-30 15:55:09 +0530

    Update 1 -10 and 10 -1.py

--- PRACTICE PGM/1 -10 and 10 -1.py
+++ PRACTICE PGM/1 -10 and 10 -1.py
@@ -1,5 +1,16 @@
-n = 11
-for i in range(1,11):
-     print(i , end='')
-     print(' ',n-i)
+# n = 11
+# for i in range(1,11):
+#      print(i , end='')
+#      print(' ',n-i)
 
+# a,b=1,10
+# while (a !=11):
+#     print(a,b)
+#     a+=1
+#     b-=1
+
+a,b = 1,10
+for i in range(a,b):
+    print('{}{}'.format(a,b))
+    a+=1
+    b-=1
