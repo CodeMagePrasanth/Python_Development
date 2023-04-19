1e538dc9e2da1ea0146457bfac4d9a662ac3bd86
prasanth <prasanthbr32@gmail.com>
2023-04-19 21:53:06 +0530

    C:\Users\Dreams Tattooing\Desktop\Jspiders\Core python\python\PROGRAMS\Function in python\1.Prime number in funtion.py

--- PROGRAMS/Function in python/3.Armstrong number.py
+++ PROGRAMS/Function in python/3.Armstrong number.py
@@ -1,14 +1,13 @@
-def arm(num):
+def arm(num,p):
     add = 0
-    count = len(str(num))
     while num != 0:
         rem = num % 10
-        add += rem ** count
+        add += rem ** p
         num //= 10
     return add
 
 num = 153
-if num ==arm(num):
+if (num == arm(num,len(str(num)))):
     print('arm strong number')
 else:
     print('not armstrong number ')
--- PROGRAMS/Function in python/6.disairum.py
+++ PROGRAMS/Function in python/6.disairum.py
@@ -1,15 +1,14 @@
-def disarium(num):
+def disarium(num,p):
     add = 0
-    count = len(str(num))
     while num != 0:
         rem = num % 10
-        add += rem ** count
+        add += rem ** p
         num //= 10
-        count-=1
+        p-=1
     return add
 
 num = 135
-if num == disarium(num):
+if (num == disarium(num,len(str(num)))):
     print('Disarium number')
 else:
     print('not Disarium number ')
