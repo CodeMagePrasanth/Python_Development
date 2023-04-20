6cf042ade80345401ee977e5dd7b91c44d6259a8
prasanth <prasanthbr32@gmail.com>
2023-04-20 16:42:05 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	PROGRAMS/Numbers programs/autoMorphic number.py

--- PROGRAMS/Function in python/1.even number.py
+++ PROGRAMS/Function in python/1.even number.py
@@ -1,14 +1,15 @@
-# Funtion using programs
+# Function using programs
 
 # Even Number :
-# ---> given number is divisible with 2 or onther numbers %2
+# ---> given number is divisible with 2 or other numbers %2
 # ---> if given number is divided Return True
 # ---> other wise  Return False.
 
 def even(num):
-    if num%2 == 0 :
-       return True
+    if num % 2 == 0:
+        return True
     return False
+
 
 print(even(2))
 print(even(5))
