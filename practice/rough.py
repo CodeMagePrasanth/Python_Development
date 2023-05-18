c71adceae2e0719b22a94a681e0b9bb4d6906f6e
prasanth <prasanthbr32@gmail.com>
2023-05-18 22:30:51 +0530

    Update workspace.xml

--- practice/rough.py
+++ practice/rough.py
@@ -1,5 +1,14 @@
-l = [10,20,30,40,50]
-res =[]
-for i in range(4,-1,-1):
-    res += l.append(i)
-print(res)
+# perfect
+#     1
+#   1 1 1
+# 1 1 1 1 1
+#   1 1 1
+#     1
+
+n = 3
+a = 1
+for i in range(1,n+1):
+    print('{}{}{}'.format('  '*(n-i),'* '*(i),'* '*(i-1)))
+    a +=1
+for i in range(2,0,-1):
+    print('{}{}{}'.format('  '*(n-i),'* '*(i),'* '*(i-1)))
