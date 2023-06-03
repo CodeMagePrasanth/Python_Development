6a66c03fd5c05b299244b70943c2c9e5cfa55514
prasanth <prasanthsenthilkumaran@gmail.com>
2023-06-03 11:21:14 +0530

    Update rough.py

--- practice/rough.py
+++ practice/rough.py
@@ -8,3 +8,16 @@
 
 a,b,c,d =(1,2,3,4,5)
 print(a,b,c,d)
+
+
+a,b,c,d =(1,2,3,4,5)
+print(a,b,c,d)
+
+a,b,c,d =(1,2,3,4,5)
+print(a,b,c,d)
+a,b,c,d =(1,2,3,4,5)
+print(a,b,c,d)
+a,b,c,d =(1,2,3,4,5)
+print(a,b,c,d)
+a,b,c,d =(1,2,3,4,5)
+print(a,b,c,d)
