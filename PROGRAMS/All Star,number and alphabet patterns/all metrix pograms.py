3d23aa8369758b4b61686abae25cc025be027aa9
prasanth <prasanthsenthilkumaran@gmail.com>
2023-08-20 19:57:55 +0530

    Update demo.py

---
+++ PROGRAMS/All Star,number and alphabet patterns/all metrix pograms.py
@@ -0,0 +1,24 @@
+# All the metrix programs
+
+#1 3X3 metrix
+
+L=[[1,2,3],[4,5,6],[7,8,9]]
+for i in range(0,3):
+    for j in range(0,3):
+        print(L[i][j],end=' ')
+    print()
+'''
+o/p 
+1 2 3 
+4 5 6 
+7 8 9 
+'''
+#2 90^ right side
+
+n=3
+f=0
+s=0
+for i in range(n):
+    for j in range(n):
+        print([j][i],end=' ')
+    print()
